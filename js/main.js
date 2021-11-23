9cecd09f69d8f042c9989993d313add0c0a167de
Minh-Tuan-2611 <tuandoanminh482@gmail.com>
2021-11-23 21:59:49 +0700

    123

---
+++ js/main.js
@@ -0,0 +1,140 @@
+import { GAME_STATUS, PAIRS_COUNT, GAME_TIME } from './constants.js'
+import { getColorListElement, getPlayAgainButton, getColorElementList } from './selectors.js';
+import { getRandomColorPairs, createTimer, showPlayAgainButton, hidePlayAgainButton, setTimerText } from './utils.js';
+import { getInActiveColorList, getColorBackground } from './selectors.js';
+
+// Global variables
+let selections = []
+let gameStatus = GAME_STATUS.PLAYING
+var timer = createTimer({
+    seconds: GAME_TIME,
+    onChange: handleTimerChange,
+    onFinish: handleTimerFinish,
+})
+
+function handleTimerChange(second) {
+    var fullSecond = `0${second}`.slice(-2);
+    setTimerText(fullSecond)
+}
+
+function handleTimerFinish() {
+    gameStatus = GAME_STATUS.FINISHED
+
+    showPlayAgainButton();
+
+    setTimerText('GAME OVER :(');
+}
+
+function handleColorClick(liElement) {
+
+    var shouldBlockClick = [GAME_STATUS.BLOCKING, GAME_STATUS.FINISHED].includes(gameStatus);
+
+    if (shouldBlockClick) {
+        return;
+    }
+
+    liElement.classList.add('active');
+    selections.push(liElement);
+    if (selections.length < 2) {
+        return;
+    }
+    var firstColor = selections[0].dataset.color;
+    var secondColor = selections[1].dataset.color;
+
+    var isMatch = firstColor === secondColor;
+
+    if (isMatch) {
+        setBackgroundColor(firstColor);
+        var isWin = getInActiveColorList().length === 0;
+        if (isWin) {
+            showPlayAgainButton();
+            setTimerText('YOU WIN :)');
+            timer.clear();
+
+            gameStatus = GAME_STATUS.FINISHED;
+        }
+
+        selections = [];
+        return;
+    }
+
+    gameStatus = GAME_STATUS.BLOCKING
+
+    setTimeout(function() {
+        selections[0].classList.remove('active');
+        selections[1].classList.remove('active');
+        selections = [];
+
+        if (gameStatus !== GAME_STATUS.FINISHED) {
+            gameStatus = GAME_STATUS.PLAYING;
+        }
+    }, 300);
+
+}
+
+function initColors() {
+    var colorList = getRandomColorPairs(PAIRS_COUNT);
+
+    var liList = getColorListElement().querySelectorAll('.overlay');
+
+    for (var index = 0; index < liList.length; index++) {
+        liList[index].parentElement.dataset.color = colorList[index];
+        var overlayElement = liList[index];
+        if (overlayElement) {
+            overlayElement.style.backgroundColor = colorList[index];
+        }
+    }
+}
+
+function attachEventForColorsList() {
+    var ulElement = getColorListElement();
+    ulElement.addEventListener('click', function(event) {
+        if (event.target.tagName !== 'LI') {
+            return;
+        }
+        handleColorClick(event.target);
+    })
+}
+
+function resetGame() {
+    gameStatus = GAME_STATUS.PLAYING;
+    selections = [];
+    var colorList = getColorElementList();
+    for (var color of colorList) {
+        color.classList.remove('active');
+    }
+    hidePlayAgainButton();
+    setTimerText('');
+    initColors();
+    startTimer();
+}
+
+function attachEventForPlayAgainButton() {
+    var playAgainButton = getPlayAgainButton();
+    playAgainButton.addEventListener('click', resetGame);
+}
+
+function setBackgroundColor(color) {
+    const backgroundElement = getColorBackground()
+    if (backgroundElement) backgroundElement.style.backgroundColor = color
+}
+
+function startTimer() {
+    timer.start();
+}
+
+
+initColors();
+
+attachEventForColorsList();
+
+setBackgroundColor('goldenrod');
+
+attachEventForPlayAgainButton();
+
+var startBtn = document.querySelector('.game__button-start');
+startBtn.classList.add('show');
+startBtn.onclick = function() {
+    startBtn.classList.remove('show');
+    startTimer();
+}
---
+++ js/utils.js
@@ -0,0 +1,76 @@
+import { getPlayAgainButton } from "./selectors.js";
+import { getTimerElement } from "./selectors.js";
+
+function shuffle(array) {
+    for (var i = array.length - 1; i > 1; i--) {
+        var j = Math.floor(Math.random() * i);
+
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+export const getRandomColorPairs = function(count) {
+
+    var colorList = [];
+
+    var hueList = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'monochrome'];
+
+    for (var i = 0; i < count; i++) {
+        var color = randomColor({
+            luminosity: 'dark',
+            hue: hueList[i % hueList.length],
+        });
+        colorList.push(color);
+    }
+
+    var fullColorList = [...colorList, ...colorList];
+
+    shuffle(fullColorList);
+
+    return fullColorList;
+}
+
+export function showPlayAgainButton() {
+    var playAgainButton = getPlayAgainButton();
+    playAgainButton.classList.add('show');
+}
+export function hidePlayAgainButton() {
+    var playAgainButton = getPlayAgainButton();
+    playAgainButton.classList.remove('show');
+}
+export function setTimerText(text) {
+    var timerElement = getTimerElement();
+    timerElement.textContent = text;
+}
+
+export function createTimer({ seconds, onChange, onFinish }) {
+    var intervalId = null;
+
+    function start() {
+        clear();
+
+        var currentSeconds = seconds;
+
+        intervalId = setInterval(function() {
+            onChange(currentSeconds);
+
+            currentSeconds--;
+            if (currentSeconds < 0) {
+                clear();
+
+                onFinish();
+            }
+        }, 1000)
+    }
+
+    function clear() {
+        clearInterval(intervalId);
+    }
+
+    return {
+        start,
+        clear
+    }
+}
